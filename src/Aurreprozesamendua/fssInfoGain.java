fa18c284695cae7b700bcebc065029b91b02ccb5
xabier01 <xabieraramburu3@gmail.com>
2024-04-08 00:16:12 +0200

    AttributeSelection, InfoGainAttributeEval y Ranker (hay que mirarlo)
    Signed-off-by: xabier01 <xabieraramburu3@gmail.com>

--- src/Aurreprozesamendua/fssInfoGain.java
+++ src/Aurreprozesamendua/fssInfoGain.java
@@ -1,5 +1,7 @@
 package Aurreprozesamendua;
 
+import weka.attributeSelection.InfoGainAttributeEval;
+import weka.attributeSelection.Ranker;
 import weka.core.Instances;
 import weka.core.converters.ArffSaver;
 import weka.core.converters.ConverterUtils;
@@ -7,19 +9,27 @@ import weka.filters.Filter;
 import weka.filters.supervised.attribute.AttributeSelection;
 
 import java.io.File;
+import java.util.Random;
 
 public class fssInfoGain {
     public static void main(String[] args) throws Exception {
         ConverterUtils.DataSource source = new ConverterUtils.DataSource(args[0]);
         Instances data = source.getDataSet();
+        //TODO
         data.setClassIndex(data.numAttributes()-1);
         System.out.println("Atributu kopurua train: " + (data.numAttributes() - 1));
         AttributeSelection attributeSelection = new AttributeSelection();
+        InfoGainAttributeEval eval = new InfoGainAttributeEval();
+        attributeSelection.setEvaluator(eval);
+        Ranker ranker = new Ranker();
+        //TODO
+        ranker.setNumToSelect(2000);
+        attributeSelection.setSearch(ranker);
         attributeSelection.setInputFormat(data);
-        data = Filter.useFilter(data, attributeSelection);
-        System.out.println("Atributu kopurua berria train: " + (data.numAttributes() - 1));
+        Instances filteredData = Filter.useFilter(data, attributeSelection);
+        System.out.println("Atributu kopurua berria train: " + (filteredData.numAttributes() - 1));
         ArffSaver saver = new ArffSaver();
-        saver.setInstances(data);
+        saver.setInstances(filteredData);
         saver.setFile(new File(args[1]));
         saver.writeBatch();
     }
